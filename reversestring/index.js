e240253fe1d6b19e55c0afd03bf449b88eb38ce3
Sumi <qkobr94@gmail.com>
2022-07-27 15:49:37 +0900

    feat(reverseString): fix reverse string problem with reverse and reduce built-in func

--- reversestring/index.js
+++ reversestring/index.js
@@ -6,6 +6,12 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse(str) {
+//   return str.split("").reverse().join("");
+// }
+
+function reverse(str) {
+  return str.split("").reduce((p, cur) => cur + p, "");
+}
 
 module.exports = reverse;
