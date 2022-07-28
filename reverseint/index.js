b9987eeb5ffb6b48bcaebab3aa84cf8364332c58
Sumi <qkobr94@gmail.com>
2022-07-28 14:16:18 +0900

    test

--- reverseint/index.js
+++ reverseint/index.js
@@ -8,6 +8,28 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+const reverse = require("../reversestring");
+
+// function reverseInt(n) {
+//   if (n > 0) {
+//     const intString = n + "";
+//     return parseFloat(intString.split("").reverse().join(""));
+//   } else if (n === 0) {
+//     return 0;
+//   } else {
+//     const intString = n + "";
+//     const str = intString.split("").reverse().join("");
+//     const str2 = "-" + str;
+//     return parseFloat(str2);
+//   }
+// }
+
+function reverseInt(n) {
+  const sign = Math.sign(n) === 1 ? 1 : -1;
+  const str = n + "";
+  if (n !== 0) {
+    return sign * parseInt(str.split("").reverse().join(""));
+  } else return 0;
+}
 
 module.exports = reverseInt;
