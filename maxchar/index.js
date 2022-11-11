bcb6d1707939e03aae566595817855aac627827e
Sumi <qkobr94@gmail.com>
2022-11-11 18:11:46 +0900

    solve char-max

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,31 +5,39 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
+// function maxChar(str) {
+//   const chars = {};
+//   //   let objs = [];
+//   let max = 0;
+//   let maxChar = "";
+//   for (let char of str) {
+//     chars[char] = chars[char] + 1 || 1;
+//   }
+
+//   console.log(chars);
+
+//   //   for (let [k, o] of Object.entries(chars)) {
+//   //     objs.push(o);
+//   //     const maxNum = Math.max(...objs);
+//   //     if (maxNum === o) {
+//   //       return k;
+//   //     }
+//   //   }
+//   for (let c in chars) {
+//     if (chars[c] > max) {
+//       max = chars[c];
+//       maxChar = c;
+//     }
+//     return maxChar;
+//   }
+// }
+
 function maxChar(str) {
-  const chars = {};
-  //   let objs = [];
-  let max = 0;
-  let maxChar = "";
+  const charObj = {};
   for (let char of str) {
-    chars[char] = chars[char] + 1 || 1;
+    charObj[char] = charObj[char] + 1 || 1;
   }
-
-  console.log(chars);
-
-  //   for (let [k, o] of Object.entries(chars)) {
-  //     objs.push(o);
-  //     const maxNum = Math.max(...objs);
-  //     if (maxNum === o) {
-  //       return k;
-  //     }
-  //   }
-  for (let c in chars) {
-    if (chars[c] > max) {
-      max = chars[c];
-      maxChar = c;
-    }
-    return maxChar;
-  }
+  const arr = Object.values(charObj);
+  return Object.keys(charObj).find((c) => charObj[c] === Math.max(...arr));
 }
-
 module.exports = maxChar;
