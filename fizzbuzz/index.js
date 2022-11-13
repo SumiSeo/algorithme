8d4e74c65c1c33530bf0e9391a684afd8ea46147
Sumi <qkobr94@gmail.com>
2022-11-13 21:02:55 +0900

    solve fizzbuzz with modulo

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -12,19 +12,29 @@
 //   4
 //   buzz
 
+// function fizzBuzz(n) {
+//   for (let i = 1; i <= n; i++) {
+//     //Is the number a multiple of 3 and 5 ?
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       console.log("fizzbuzz");
+//     } else if (i % 5 === 0) {
+//       console.log("buzz");
+//     } else if (i % 3 === 0) {
+//       console.log("fizz");
+//     } else {
+//       console.log(i);
+//     }
+//   }
+// }
 function fizzBuzz(n) {
   for (let i = 1; i <= n; i++) {
-    //Is the number a multiple of 3 and 5 ?
     if (i % 3 === 0 && i % 5 === 0) {
       console.log("fizzbuzz");
     } else if (i % 5 === 0) {
       console.log("buzz");
     } else if (i % 3 === 0) {
       console.log("fizz");
-    } else {
-      console.log(i);
-    }
+    } else console.log(i);
   }
 }
-
 module.exports = fizzBuzz;
