3e5b3a70dfe9123c3d4afc755c8202c60ac4b657
Sumi <qkobr94@gmail.com>
2022-11-22 15:51:50 +0900

    solve fibonacci with recurseive and memoization

--- fib/index.js
+++ fib/index.js
@@ -29,16 +29,43 @@
 // }
 
 // const fib = memoize(slowFib);
-function fib(n) {
-  const arr = [];
-  arr[0] = 0;
-  arr[1] = 1;
-  for (let i = 0; i < n + 1; i++) {
-    if (i !== 0 && i !== 1) {
-      arr[i] = arr[i - 1] + arr[i - 2];
+// function fib(n) {
+//   const arr = [0, 1];
+
+//   for (let i = 2; i < n + 1; i++) {
+//     arr[i] = arr[i - 1] + arr[i - 2];
+//   }
+//   return arr[n];
+// }
+
+//recursive solution
+// function fib(n) {
+//   if (n < 2) {
+//     return n;
+//   }
+//   return fib(n - 1) + fib(n - 2);
+// }
+
+//memoization
+function memoize(fn) {
+  const cache = {};
+  return function (...args) {
+    if (cache[args]) {
+      return cache[args];
     }
-  }
-  return arr[n];
+    const result = fn.apply(this, args);
+    cache[args] = result;
+    return result;
+  };
 }
+
+function slowFib(n) {
+  if (n < 2) {
+    return n;
+  }
+  return fib(n - 1) + fib(n - 2);
+}
+
+const fib = memoize(slowFib);
 
 module.exports = fib;
